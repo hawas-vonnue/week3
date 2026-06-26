63e7dfaea0e2ca414c2ef6d79aafd551069aa0cf
Hawas backer <hawas.backer@vonnue.com>
2026-06-26 15:43:21 +0530

    feat: complete week3day3task4 - Demonstrated pure functions
    - Wrote impure and pure updateUser function
    - Built five step pipeline - parseCSV, validateRows, transformRows, filterInvalid, formatOutput
    - Created a deepFreeze function

--- day3/functions.js
+++ day3/functions.js
@@ -11,6 +11,7 @@ function pureUpdateUser(users, id, changes) {
   let arr = users.slice(0, users.length);
   console.log(arr);
   arr.splice(id, 1, changes);
+
   return arr;
 }
 let arr = pureUpdateUser(users, 1, "java");
@@ -29,16 +30,62 @@ function parseCSV(data) {
     }
     csv.push(obj);
   }
+
   return csv;
 }
 
-function validateRows(parsedCsv) {}
+function validateRows(parsedCsv) {
+  //to deep clone the array
+  csv = structuredClone(parsedCsv);
+  csv.forEach((obj) => {
+    if (obj["Age"] <= 0) {
+      obj["valid"] = false;
+    } else obj["valid"] = true;
+  });
+
+  return csv;
+}
+
+function transformRows(validatedCsv) {
+  csv = structuredClone(validatedCsv);
+  csv.forEach((obj) => {
+    if (typeof obj["Age"] !== "Number") {
+      obj["Age"] = Number(obj["Age"]);
+    }
+  });
+  return csv;
+}
+
+function filterInvalid(transformedCsv) {
+  csv = structuredClone(transformedCsv);
+  for (let i = 0; i < csv.length; i++) {
+    if (!csv[i]["valid"]) {
+      csv.splice(i, 1);
+    }
+  }
+  return csv;
+}
+
+function formatOutput(filteredCsv) {
+  csv = structuredClone(filteredCsv);
+  csv.forEach((obj) => {
+    delete obj.valid;
+  });
+  return csv;
+}
 fs.readFile("csv.txt", (err, data) => {
   if (err) throw err;
   let csv = data.toString();
+  console.log("before five-step pipeline");
+  console.log(csv);
+  //stored to seperate variables to show they are pure functions
   let parsedCsv = parseCSV(csv);
-  // console.log(parsedCsv);
-  // console.log(validateRows(csv));
+  let validatedCsv = validateRows(parsedCsv);
+  let transformedCsv = transformRows(validatedCsv);
+  let filteredCsv = filterInvalid(transformedCsv);
+  let formatedOutput = formatOutput(filteredCsv);
+  console.log("formated output:");
+  console.log(formatedOutput);
 });
 
 function deepFreeze(obj) {
@@ -48,6 +95,7 @@ function deepFreeze(obj) {
       deepFreeze(obj[property]),
     );
   }
+
   return obj;
 }
 let myObj3 = {
