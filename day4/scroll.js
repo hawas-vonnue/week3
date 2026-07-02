5c19934a398b84fdb1cbc543c618c2394faccd71
Hawas backer <hawas.backer@vonnue.com>
2026-07-02 11:53:59 +0530

    feat: complete week3day4task7-Create Infinite scroll Blog Feed
    - fetch 10 posts on load
    - added intersection observer to make infinite scroll until 100 posts
    - Showed loading spinner during fetch
    - Showed end of feed after 100 posts are loaded
    - Handled mid scroll errors with retry button

---
+++ day4/scroll.js
@@ -0,0 +1,93 @@
+async function fetchPosts(url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("error in fetching");
+    }
+    const responseJson = await response.json();
+    return responseJson;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+let N = 0;
+const feedElement = document.querySelector(".feed");
+const spinnerContainer = document.querySelector(".spinnerContainer");
+const retryButton = document.querySelector(".retryButton");
+
+function onLoad() {
+  if (N >= 100) {
+    const endElement = document.querySelector(".end");
+    endElement.textContent = "End of feed";
+    spinnerContainer.classList.add("hidden");
+    retryButton.classList.add("hidden");
+    return;
+  }
+  let url = `https://jsonplaceholder.typicode.com/posts?_start=${N}&_limit=10`;
+  if (N === 30) {
+    url = `https://jsonplaceholder.typicode.com/post?_start=${N}&_limit=10`;
+  }
+  fetchPosts(url).then(
+    (response) => {
+      for (let i = 0; i < 10; i++) {
+        let postElement = createPost(
+          response[i].title,
+          response[i].body,
+          response[i].userId,
+          response[i].id,
+        );
+        feedElement.appendChild(postElement);
+      }
+      N = N + 10;
+      retryButton.classList.add("hidden");
+    },
+    (error) => {
+      spinnerContainer.classList.add("hidden");
+      retryButton.classList.remove("hidden");
+      //to avoid intersection observer calling the onLoad function
+      sentinelElement.classList.add("hidden");
+    },
+  );
+}
+
+function createPost(title, body, userId, id) {
+  const postElement = document.createElement("div");
+  postElement.classList.add("post");
+  const titleElement = document.createElement("span");
+  titleElement.classList.add("title");
+  titleElement.textContent = title;
+  const bodyElement = document.createElement("span");
+  bodyElement.classList.add("body");
+  bodyElement.textContent = body;
+  const userIdElement = document.createElement("span");
+  userIdElement.classList.add("userId");
+  userIdElement.textContent = "user id:" + userId;
+  const topElement = document.createElement("div");
+  topElement.classList.add("top");
+  const idElement = document.createElement("span");
+  idElement.classList.add("id");
+  idElement.textContent = id;
+  topElement.appendChild(idElement);
+  topElement.appendChild(titleElement);
+  postElement.appendChild(topElement);
+  postElement.appendChild(userIdElement);
+  postElement.appendChild(bodyElement);
+
+  return postElement;
+}
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      spinnerContainer.classList.remove("hidden");
+      onLoad();
+    }
+  });
+});
+const sentinelElement = document.querySelector(".sentinel");
+observer.observe(sentinelElement);
+retryButton.addEventListener("click", () => {
+  spinnerContainer.classList.remove("hidden");
+  N = N + 10;
+  onLoad();
+  sentinelElement.classList.remove("hidden");
+});
