7e6663413befe5effed4228cbe72edcbbd907416
Hawas backer <hawas.backer@vonnue.com>
2026-07-03 17:01:07 +0530

    feat: complete week3day5task2-Wired All core features
    - Added mobile drawer
    - Added accordion based faq on services page
    - Lightbox on teams page
    - Scroll animation on home page
    - Reading progress bar on home page
    - Back to top button across all pages

--- day5/js/main.js
+++ day5/js/main.js
@@ -1,6 +1,38 @@
+import { openDrawer } from "./components/nav.js";
 import { darkModeToggle } from "./components/darkMode.js";
+import { accordion } from "./components/accordion.js";
+import { lightBox } from "./components/lightbox.js";
+import { scrollAnimation } from "./components/scroll.js";
+import { progressBar } from "./components/progress.js";
 
 window.onload = (event) => {
   let toggleElement = document.querySelector("#checkbox");
-  darkModeToggle(toggleElement)
+  darkModeToggle(toggleElement);
+  let hamburgerElement = document.querySelector("#menu");
+  let drawerElement = document.querySelector(".drawer");
+  openDrawer(hamburgerElement, drawerElement);
+  const faqs = document.querySelectorAll(".faqSection .faq");
+  faqs.forEach((faq) => {
+    const questionElement = faq.querySelector(".question");
+    const answerElement = faq.querySelector(".answer");
+    accordion(questionElement, answerElement);
+  });
+  if (window.location.href.includes("team.html")) {
+    lightBox();
+  }
+  if (window.location.href.includes("index.html")) {
+    scrollAnimation();
+  }
+  const backToTopButton = document.querySelector(".back-to-top");
+  window.addEventListener("scroll", () => {
+    if (scrollY >= 300) {
+      backToTopButton.style.display = "block";
+    } else {
+      backToTopButton.style.display = "none";
+    }
+  });
+  backToTopButton.addEventListener("click", (event) => {
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  });
+  progressBar("index.html");
 };
