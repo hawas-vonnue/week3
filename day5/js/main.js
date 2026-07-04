2cb79e46da1348a6520ff3612b5dd3841ed4ffce
Hawas backer <hawas.backer@vonnue.com>
2026-07-04 17:50:10 +0530

    feat: complete week3day5task5-polished and deployed the project

--- day5/js/main.js
+++ day5/js/main.js
@@ -13,71 +13,71 @@ import { renderUsers } from "./components/users.js";
 import { renderRecentPosts } from "./components/recentPosts.js";
 
 window.onload = (event) => {
-  let toggleElement = document.querySelector("#checkbox");
-  darkModeToggle(toggleElement);
-  let hamburgerElement = document.querySelector("#menu");
-  let drawerElement = document.querySelector(".drawer");
-  openDrawer(hamburgerElement, drawerElement);
-  const faqs = document.querySelectorAll(".faqSection .faq");
-  faqs.forEach((faq) => {
-    const questionElement = faq.querySelector(".question");
-    const answerElement = faq.querySelector(".answer");
-    accordion(questionElement, answerElement);
-  });
-  if (window.location.href.includes("team.html")) {
-    renderUsers();
-  }
-  if (window.location.href.includes("index.html")) {
-    scrollAnimation();
-    renderRecentPosts();
-  }
-  const backToTopButton = document.querySelector(".back-to-top");
-  window.addEventListener("scroll", () => {
-    if (scrollY >= 300) {
-      backToTopButton.style.display = "block";
-    } else {
-      backToTopButton.style.display = "none";
-    }
-  });
-  backToTopButton.addEventListener("click", (event) => {
-    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
-  });
-  progressBar("index.html");
-  if (window.location.href.includes("contact.html")) {
-    const form = document.querySelector("form");
-    const formValidator = new FormValidator(form, rules);
-    const spinner = document.querySelector(".spinnerContainer");
-    const submitButton = document.querySelector("#submitButton");
-    form.addEventListener(
-      "blur",
-      (event) => {
-        event.preventDefault();
-        formValidator.validate(event.target);
-      },
-      true,
-    );
-    form.addEventListener("submit", (event) => {
-      event.preventDefault();
-      submitButton.style.display = "none";
-      spinner.style.display = "inline-block";
-      setTimeout(() => {
-        let flag = formValidator.validateAll();
-        submitButton.style.display = "revert";
-        spinner.style.display = "none";
-        if (flag === 1) showToast("Error", 7, "error");
-        else {
-          showToast("success", 7, "success");
-          form.reset();
-          const fields = form.querySelectorAll("input,textarea");
-          fields.forEach((field) => {
-            field.classList.remove("is-valid");
-          });
-        }
-      }, 1500);
+    let toggleElement = document.querySelector("#checkbox");
+    darkModeToggle(toggleElement);
+    let hamburgerElement = document.querySelector("#menu");
+    let drawerElement = document.querySelector(".drawer");
+    openDrawer(hamburgerElement, drawerElement);
+    const faqs = document.querySelectorAll(".faqSection .faq");
+    faqs.forEach((faq) => {
+        const questionElement = faq.querySelector(".question");
+        const answerElement = faq.querySelector(".answer");
+        accordion(questionElement, answerElement);
     });
-  }
-  if (window.location.href.includes("services.html")) {
-    fetchPosts();
-    searchFunction();
-  }
+    if (window.location.href.includes("team.html")) {
+        renderUsers();
+    }
+    if (window.location.href.includes("index.html")) {
+        scrollAnimation();
+        renderRecentPosts();
+    }
+    const backToTopButton = document.querySelector(".back-to-top");
+    window.addEventListener("scroll", () => {
+        if (scrollY >= 300) {
+            backToTopButton.style.display = "block";
+        } else {
+            backToTopButton.style.display = "none";
+        }
+    });
+    backToTopButton.addEventListener("click", (event) => {
+        window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+    });
+    progressBar("index.html");
+    if (window.location.href.includes("contact.html")) {
+        const form = document.querySelector("form");
+        const formValidator = new FormValidator(form, rules);
+        const spinner = document.querySelector(".spinnerContainer");
+        const submitButton = document.querySelector("#submitButton");
+        form.addEventListener(
+            "blur",
+            (event) => {
+                event.preventDefault();
+                formValidator.validate(event.target);
+            },
+            true
+        );
+        form.addEventListener("submit", (event) => {
+            event.preventDefault();
+            submitButton.style.display = "none";
+            spinner.style.display = "inline-block";
+            setTimeout(() => {
+                let flag = formValidator.validateAll();
+                submitButton.style.display = "revert";
+                spinner.style.display = "none";
+                if (flag === 1) showToast("Error", 7, "error");
+                else {
+                    showToast("success", 7, "success");
+                    form.reset();
+                    const fields = form.querySelectorAll("input,textarea");
+                    fields.forEach((field) => {
+                        field.classList.remove("is-valid");
+                    });
+                }
+            }, 1500);
+        });
+    }
+    if (window.location.href.includes("services.html")) {
+        fetchPosts();
+        searchFunction();
+    }
 };
